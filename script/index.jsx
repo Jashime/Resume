bb103dc6d9c868abc77697a009f0fb8778c4b4ad
Jashime <hhf0804@outlook.com>
2018-03-17 22:24:45 +0800

    3.17

--- script/index.jsx
+++ script/index.jsx
@@ -1,1 +1,12 @@
-console.log('resume ggffddg');
+var React = require('react');
+var ReactDOM =require('react-dom');
+
+var NavBar = React.createClass({
+    render:function(){
+        return(
+            <div></div>
+        )
+    }
+})
+
+// ReactDOM.render(<NavBar/>,document.getElementById('welcome-page'));
